4136b12ca9d484e914e267ce4a40782c93c82082
PATRICK GUIMARAES CORREIA <patrickguimaraescorreia@gmail.com>
2025-02-20 20:08:55 -0300

    Estrutura do projeto

--- main.js
+++ main.js
@@ -6,11 +6,33 @@
 // Importação do modulo de conexão (database.js)
 const { conectar, desconectar } = require('./database.js')
 
-// execução da aplicação
-const app = async() => {
-    await conectar()
-    await desconectar()
+// Importação do modelo de dados de clientes
+const clienteModel = require('./src/models/Clientes.js')
+
+// CRUD Create (função para adicionar um novo cliente)
+const criarCliente = async (nomeCli, foneCli) => {
+    try {
+        const novoCliente = new clienteModel(
+            {
+                nomeCliente: nomeCli,
+                foneCliente: foneCli
+            }
+        )
+        // a linha abaixo salva os dados do cliente no banco
+        await novoCliente.save()
+        console.log("Cliente adicionado com sucesso")
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
+// execução da aplicação
+const app = async () => {
+    await conectar()
+    await criarCliente("Patrick G", "11971263806")
+    await criarCliente("Ana Carolina", "11871456208")
+    await desconectar()
+}
 console.clear()
 app()
---
+++ src/models/Clientes.js
@@ -0,0 +1,20 @@
+/**
+ * Clientes
+ */
+
+// importação da biblioteca
+const { model, Schema } = require('mongoose')
+
+//criação da estrutura de dados ("coleção") que sera usada no banco
+const clienteSchema = new Schema({
+    nomeCliente: {
+        type: String
+    },
+    foneCliente: {
+        type: String
+    }
+}, { versionKey: false })
+
+// importação do modelo de dados
+// observação clientes  sera o nome da coleção
+module.exports = model('Clientes', clienteSchema)
